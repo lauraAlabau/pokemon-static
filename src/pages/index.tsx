c78203509e3291d188b1e869504c0d34d9ea243f
lauraAlabau <arlaura3@gmail.com>
2023-04-01 13:58:38 +0200

    feature: black theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@nextui-org/react";
 import { NextPage } from "next";
 
 const HomePage: NextPage = () => {
   return (
     <>
       <h1>Home Page</h1>
+      <Button color={"gradient"}>Click me</Button>
     </>
   );
 };
